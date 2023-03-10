1f71bdd17fe4bd9a3e76763e7ec41c882ff17785
joshuapent <joshdavidpent@gmail.com>
2023-03-10 08:56:24 -0600

    Setup for controllers and server.js done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const employeesController = require('./controllers/employees.js')
 
 const methodOverride = require('method-override');
 
@@ -11,6 +12,25 @@ app.use(express.urlencoded({extended: false}));
 
 app.use(methodOverride('_method'));
 
+app.use('/employees', employeesController)
+
+app.get('/', (req, res, next) => {
+    try {
+        res.render('main');
+    } catch(err) {
+        console.log(err);
+        return next();
+    }
+})
+
+app.get('/*', (req, res, next) => {
+    try {
+        res.render('404')
+    } catch(err) {
+        console.log(err);
+        return next();
+    }
+})
 
 app.listen('4000', () => {
     console.log('Listening on port 4000')
